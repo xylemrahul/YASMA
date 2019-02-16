a90c32906cb238a59e696486abd865ba32fd079e
xylemrahul <rajspeed.20@gmail.com>
2019-02-17 00:29:59 +0530

    Added api response model files

---
+++ app/src/main/java/com/example/yasma/model/AlbumPhoto.java
@@ -0,0 +1,60 @@
+
+package com.example.yasma.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class AlbumPhoto {
+
+    @SerializedName("albumId")
+    private Integer albumId;
+    @SerializedName("id")
+    private Integer id;
+    @SerializedName("title")
+    private String title;
+    @SerializedName("url")
+    private String url;
+    @SerializedName("thumbnailUrl")
+    private String thumbnailUrl;
+
+    public Integer getAlbumId() {
+        return albumId;
+    }
+
+    public void setAlbumId(Integer albumId) {
+        this.albumId = albumId;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+}
---
+++ app/src/main/java/com/example/yasma/model/Geo.java
@@ -0,0 +1,30 @@
+
+package com.example.yasma.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Geo {
+
+    @SerializedName("lat")
+    private String lat;
+    @SerializedName("lng")
+    private String lng;
+
+    public String getLat() {
+        return lat;
+    }
+
+    public void setLat(String lat) {
+        this.lat = lat;
+    }
+
+    public String getLng() {
+        return lng;
+    }
+
+    public void setLng(String lng) {
+        this.lng = lng;
+    }
+
+}
