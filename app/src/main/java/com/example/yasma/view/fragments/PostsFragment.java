76a4ab3af494118a96a4070ea87f98bf39273a89
xylemrahul <rajspeed.20@gmail.com>
2019-02-18 01:15:40 +0530

    Loaded data onto PostsFragment

--- app/src/main/java/com/example/yasma/view/fragments/PostsFragment.java
+++ app/src/main/java/com/example/yasma/view/fragments/PostsFragment.java
@@ -1,6 +1,7 @@
 package com.example.yasma.view.fragments;
 
 import android.annotation.SuppressLint;
+import android.arch.lifecycle.ViewModelProvider;
 import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.yasma.R;
+import com.example.yasma.databinding.FragmentPostsBinding;
 import com.example.yasma.viewmodel.PostsViewModel;
 
 import java.util.Observable;
@@ -23,7 +25,7 @@ import java.util.Observer;
 public class PostsFragment extends Fragment implements Observer {
 
     private PostsViewModel postsViewModel;
-    private RecyclerView recyclerView;
+    FragmentPostsBinding postDataBinding;
 
     String TAG = PostsFragment.class.getSimpleName();
 
@@ -31,41 +33,32 @@ public class PostsFragment extends Fragment implements Observer {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        ViewDataBinding postDataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_posts, container, false);
-
+        postDataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_posts, container, false);
+        postDataBinding.listPeople.setLayoutManager(new LinearLayoutManager(getContext()));
+        postDataBinding.listPeople.setAdapter(new PostsAdapter());
         return postDataBinding.getRoot();
     }
 
-    @SuppressLint("CheckResult")
     @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         postsViewModel = new PostsViewModel();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-
-        recyclerView = view.findViewById(R.id.list_people);
-        setupListPosts(recyclerView);
-//        setupObserver(postsViewModel);
+        setupObserver(postsViewModel);
     }
 
     public void setupObserver(Observable observable) {
         observable.addObserver(this);
     }
 
-    private void setupListPosts(RecyclerView listPeople) {
-        PostsAdapter adapter = new PostsAdapter();
-        listPeople.setAdapter(adapter);
-        listPeople.setLayoutManager(new LinearLayoutManager(getContext()));
-    }
-
     @Override
     public void update(Observable observable, Object arg) {
 
-        PostsAdapter adapter = (PostsAdapter) recyclerView.getAdapter();
+        PostsAdapter adapter = (PostsAdapter) postDataBinding.listPeople.getAdapter();
         PostsViewModel postsViewModel = (PostsViewModel) observable;
         adapter.setPostsResponsesList(postsViewModel.getmPostResponse());
     }
